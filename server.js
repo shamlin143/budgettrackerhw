c61084ea03f340f13e26eb6e398acb9341b21478
shamlin143 <shamlin@gvtc.com>
2020-06-01 17:58:30 -0500

    fix5

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || process.env.REMOTE || "mongodb://localhost/budget";
 
-mongoose.connect (MONGODB_URI, {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
